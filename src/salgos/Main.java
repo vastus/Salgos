654144cfffd59b9aff46af5850c803c44e10667f
juho H <juhoth@ovi.com>
2014-01-21 23:53:16 +0200

    merge sort usage

--- src/salgos/Main.java
+++ src/salgos/Main.java
@@ -5,5 +5,12 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
+        int[] ary = {45, 32, 9229, 1, 2, 1, 29, 4, 6, 3, 9, 10};
+
+        System.out.println("Merge sort.");
+        System.out.println(String.format("Given:  %s", Arrays.toString(ary)));
+        Merge.sort(ary);
+        System.out.println(String.format("Sorted: %s", Arrays.toString(ary)));
     }
+
 }
