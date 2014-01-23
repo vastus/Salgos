c02a780c3c1726374860213ec6b83dee8f0ad8e9
juho H <juhoth@ovi.com>
2014-01-23 13:30:26 +0200

    sort comparable arrays

--- src/salgos/Merge.java
+++ src/salgos/Merge.java
@@ -60,4 +60,58 @@ public class Merge {
         while (j < b.length) ary[k++] = b[j++];
     }
 
+    /**
+     * Sorts the given array using merge sort.
+     * @param ary array to be sorted
+     */
+    public static void sort(Comparable[] ary) {
+        // Sorted if less than 2 elements.
+        if (ary.length < 2) return;
+
+        // Divide.
+        Comparable[][] s = divide(ary);
+
+        // Recur.
+        sort(s[0]); sort(s[1]);
+
+        // Conquer.
+        merge(ary, s[0], s[1]);
+    }
+
+    /**
+     * Divide the given array in two sub-arrays.
+     * @param ary array to be halved
+     * @return sub-arrays of ary
+     */
+    public static Comparable[][] divide(Comparable[] ary) {
+        int len = ary.length;
+        int mid = len / 2;
+        Comparable[] first = new Comparable[mid];
+        Comparable[] last = new Comparable[mid + (len %2)];
+
+        int i = 0, j = 0, k = 0;
+        while (i < first.length) first[i++] = ary[k++];
+        while (j < last.length) last[j++] = ary[k++];
+
+        return new Comparable[][] {first, last};
+    }
+
+    /**
+     * Merges given sub-arrays in ascending order
+     * to the array ary being sorted.
+     * @param ary array to be sorted
+     * @param a first half of ary
+     * @param b second half of ary
+     */
+    public static void merge(Comparable[] ary, Comparable[] a, Comparable[] b) {
+        // Loop until we've gone thru sub-arrays a and/or b.
+        int i = 0, j = 0, k = 0;
+        while (i < a.length && j < b.length)
+            ary[k++] = (a[i].compareTo(b[j]) < 0) ? a[i++] : b[j++];
+
+        // Loop thru the remaining elements.
+        while (i < a.length) ary[k++] = a[i++];
+        while (j < b.length) ary[k++] = b[j++];
+    }
+
 }
