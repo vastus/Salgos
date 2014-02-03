e51ccc3026e1e7cbe288d23bc84fe4cf12a84c71
Juho H <juhoth@ovi.com>
2014-02-03 22:40:40 +0200

    clean up

--- src/salgos/Main.java
+++ src/salgos/Main.java
@@ -6,65 +6,15 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-
-        int ary[] = {7, 3, 5, 0, 1, 2, 8};
-        //int expected[] = {5, 3, 2, 0, 1, 7, 8};
-        print(ary);
-        Heap.sort(ary);
-        print(ary);
-
-        /*
-        int[] a = {45, 32, 9229, 1, 2, 1, 29, 4, 6, 3, 9, 10};
-        int[] A = {45, 32, 9229, 1, 2, 1, 29, 4, 6, 3, 9, 10};
-        int[] b = {45, 32, 9229, 1, 2, 1, 29, 4, 6, 3, 9, 10, 14};
-        int[] B = {45, 32, 9229, 1, 2, 1, 29, 4, 6, 3, 9, 10, 14};
-        int[] c = {45, 32, 9229, 1, 2, 10000, 29, 4, 6, 3, 9, 10};
-        int[] C = {45, 32, 9229, 1, 2, 10000, 29, 4, 6, 3, 9, 10};
-        int[] d = {45, 32, 9229, 1, 2, 10000, 29, 4, 6, 3, 9, 10, 239};
-        int[] D = {45, 32, 9229, 1, 2, 10000, 29, 4, 6, 3, 9, 10, 239};
-
-        print(a);
-        print(A);
-        Heap.heapivai(a, a.length);
-        print(a);
-        //print(a);
-        Heap.sort(A);
-        print(A);
-
-        System.out.println("");
-        Heap.heapivai(b, b.length);
-        print(b);
-
-        Heap.sort(B);
-        print(B);
-
-        System.out.println("---------\n\n");
-        print(c);
-
-        Heap.heapivai(c, c.length);
-        print(c);
-
-        Heap.sort(C);
-        print(C);
-
-        Heap.heapivai(d, d.length);
-        print(d);
-
-        Heap.sort(D);
-        print(D);
-
-        int t[] = {0, 1, 2, 3, 4, 5};
-        print(t);
-        Heap.heapivai(t, t.length);
-        print(t);
-        */
-
-
-        /*
-        int random[] = random(10000000);
+        int random[] = random(100);
         int copy[]= Arrays.copyOf(random, random.length);
 
+        Arrays.sort(random);
+        Heap.sort(copy);
+        print(random);
+        print(copy);
 
+        /*
         long startTime = System.currentTimeMillis(); //nanoTime();
         Merge.sort(random);
         long endTime = System.currentTimeMillis(); //nanoTime();
