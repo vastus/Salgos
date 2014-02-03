b8f323ba91b777b7fa8221398f2ca84ab93d8415
Juho H <juhoth@ovi.com>
2014-02-03 22:34:00 +0200

    testing the whole heap sort

--- test/salgos/HeapTest.java
+++ test/salgos/HeapTest.java
@@ -1,8 +1,10 @@
 package salgos;
 
-import java.util.Arrays;
+import java.util.Random;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import static salgos.TestHelpers.*;
 
 /**
  * Test class for Heap.
@@ -174,5 +176,74 @@ public class HeapTest {
         assertArrayEquals(expected, ary);
     }
 
+    @Test
+    public void sortsGivenArray() {
+        int ary[] = {8, 3, 7, 0, 1, 5, 2};
+        int expected[] = {0, 1, 2, 3, 5, 7, 8};
+        Heap.sort(ary);
+        assertArrayEquals(expected, ary);
+    }
+
+    @Test
+    public void arrayOfOneElementIsSorted() {
+        int[] ary = {0};
+        int[] sorted = {0};
+        Heap.sort(ary);
+        assertArrayEquals(sorted, ary);
+    }
+
+    @Test
+    public void sortsArrayOfTwoElements() {
+        int[] ary = {1, 0};
+        int[] sorted = {0, 1};
+        Heap.sort(ary);
+        assertArrayEquals(sorted, ary);
+    }
+
+    @Test
+    public void sortsArrayOfTwoElementsII() {
+        int[] ary = {2, 1};
+        int[] sorted = {1, 2};
+        Heap.sort(ary);
+        assertArrayEquals(sorted, ary);
+    }
+
+    @Test
+    public void sortsArrayOfThreeElements() {
+         int[] ary = {2, 1, 0};
+         int[] sorted = {0, 1, 2};
+         Heap.sort(ary);
+         assertArrayEquals(sorted, ary);
+    }
+
+    @Test
+    public void sortsIntsThatAreNotAdjacent() {
+        int[] ary = {29, 12, 90, 0, -9};
+        int[] sorted = {-9, 0, 12, 29, 90};
+        Heap.sort(ary);
+        assertArrayEquals(ary, sorted);
+    }
+
+    @Test
+    public void sortsRandomNumbers() {
+        int n = 9;
+        int[] ary = new int[n];
+        for (int i = 0; i < n; i++)
+            ary[i] = new Random().nextInt();
+        assertFalse(isAscending(ary)); // this could fail...
+        Heap.sort(ary);
+        assertTrue(isAscending(ary));
+    }
+
+    @Test
+    public void sortsRandomNumbersII() {
+        int n = 14;
+        int[] ary = new int[n];
+        for (int i = 0; i < n; i++)
+            ary[i] = new Random().nextInt();
+        assertFalse(isAscending(ary)); // this could fail...
+        Heap.sort(ary);
+        assertTrue(isAscending(ary));
+    }
 
 }
