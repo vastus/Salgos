f6ed324961221111be81d331ec39a6357dbc85c5
Juho H <juhoth@ovi.com>
2014-02-05 00:09:32 +0200

    helper utils

---
+++ src/utils/Helpers.java
@@ -0,0 +1,21 @@
+package utils;
+
+/**
+ * Helper utilities class.
+ * @author Juho Hautala
+ */
+public class Helpers {
+
+    /**
+     * Swaps given array's elements from given indices.
+     * @param ary array whose elements to be swapped
+     * @param i first element's index
+     * @param j second element's index
+     */
+    public static void swap(int ary[], int i, int j) {
+        int temp = ary[i];
+        ary[i] = ary[j];
+        ary[j] = temp;
+    }
+
+}
