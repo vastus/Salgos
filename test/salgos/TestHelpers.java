e8f55f1db20d0940e15e142dfda142e31559ad4c
Juho H <juhoth@ovi.com>
2014-02-10 22:41:06 +0200

    implement isAscending using Comparable type

--- test/salgos/TestHelpers.java
+++ test/salgos/TestHelpers.java
@@ -1,9 +1,12 @@
 package salgos;
 
+/**
+ * Helper methods needed in tests.
+ * @author Juho Hautala
+ */
 public class TestHelpers {
  
     /**
-     * This is just a helper method for running some of the tests.
      * Checks that given array is sorted in ascending order.
      * @param ary array to be checked
      * @return true if sorted in ascending order, false otherwise
@@ -11,6 +14,18 @@ public class TestHelpers {
     protected static boolean isAscending(int[] ary) {
         for (int i = 0; i < (ary.length - 1); i++)
             if (ary[i] > ary[(i + 1)])
+                return false;
+        return true;
+    }
+
+    /**
+     * Checks that given array is sorted in ascending order.
+     * @param ary array to be checked
+     * @return true if sorted in ascending order, false otherwise
+     */
+    protected static boolean isAscending(Comparable[] ary) {
+        for (int i = 0; i < (ary.length - 1); i++)
+            if (ary[i].compareTo(ary[(i + 1)]) > 0)
                 return false;
         return true;
     }
